16192f04ed9e886f6ef50aa245ecf2a144601c86
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 07:32:54 -0500

    updated config.h to explain vars

--- config.h
+++ config.h
@@ -7,3 +7,13 @@
 
 #include <LilyGoWatch.h>
 
+const char* ssid = "YOUR WIFI SSID";
+const char* password = "YOUR WIFI PW";
+
+// use gps on your smartphone to get your latitude and longitude values like (example--> 29.02355 and -61.99456)
+// replace YOUR_LATITUDE in this url with your latitude (for example 29.02355)
+// replace YOUR_LONGITUDE in this url with your longitude value (for example -61.994561)
+const char* regionURL = "https://api.bigdatacloud.net/data/reverse-geocode-client/?latitude=YOUR_LATITUDE&longitude=YOUR_LONGITUDE";
+
+// contact openweathermap to get an api key and replace YOUR_API_KEY with your key which looks like abc131cba38abc321abc987abc334
+const char* oweatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=YOUR_LATITUDE&lon=YOUR_LONGITUDE&units=metric&appid=YOUR_API_KEY";
